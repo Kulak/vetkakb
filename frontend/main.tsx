5ff162508d92e22b569fc7fa11f2bdb081a070f6
Sergei G <sergeig.public@gmail.com>
2016-08-22 23:22:18 -0700

    introduced app props and state

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,18 +1,33 @@
 
-/// <reference path="./typings/modules/react/index.d.ts" />
+//  / <reference path="./typings/modules/react/index.d.ts" />
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class App extends React.Component<any> {
-  constructor() {
-  }
-  render() {
-    return <div>Hello world!</div>
-  }
+class AppProps {
+  constructor(public defaultUrl: string){}
 }
 
+class AppState {
+  constructor(public url: string) {}
+}
+
+class App extends React.Component<AppProps, AppState> {
+  // context is currently {}
+  constructor(props: AppProps, context) {
+    super(props, context)
+    this.state = new AppState(props.defaultUrl);
+  }
+  render() {
+    if (this.state.url == '/') {
+      return <div>Default page</div>
+    } else {
+      return <div>Unknown url: {this.state.url}</div>
+    }
+  }
+} // end of App class
+
 ReactDOM.render(
-  <App />,
+  <App defaultUrl="/" />,
   document.getElementById('app')
 );
