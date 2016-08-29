968c82cc56026eb20498e780b7fa4b80135b9069
Sergei G <sergeig.public@gmail.com>
2016-08-29 00:18:34 -0700

    improved user interaction
    work in pgroess with entryViewBox

--- frontend/controls/entryView.tsx
+++ frontend/controls/entryView.tsx
@@ -3,9 +3,6 @@ entryView provides standard view of the entry from
 a list of entries like search results.
 */
 import * as React from 'react'
-//import {Entry} from '../model/entry'
-//import {WSEntryPut} from '../model/wsentry'
-//import {DataService} from '../common/dataService'
 import {WSEntryGetHTML} from '../model/wsentry'
 
 export class EntryViewProps {
@@ -16,7 +13,8 @@ export class EntryViewProps {
 
 class EntryViewState {
 	constructor(
-		public expanded: boolean = false
+		public expanded: boolean = false,
+		public editing: boolean = false
 	) {}
 }
 
@@ -25,21 +23,27 @@ export class EntryViewBox extends React.Component<EntryViewProps, EntryViewState
 		super(props, context)
 		this.state = new EntryViewState();
 	}
-	onExpandClick() {
-		this.setState(new EntryViewState(true))
+	onExpandClick(expandAction: boolean) {
+		this.setState(new EntryViewState(expandAction, false))
 	}
-	onContractClick() {
-		this.setState(new EntryViewState(false))
+	onEditClick(editAction: boolean) {
+		this.setState(new EntryViewState(false, editAction))
 	}
 	render() {
 		let en = this.props.entry
-		if (this.state.expanded) {
-			return <div>
-				<h2 onClick={e => this.onContractClick()}>{en.Title}</h2>
-				<div dangerouslySetInnerHTML={{__html: en.HTML}} />
-			</div>
+		if (this.state.editing) {
+
 		} else {
-			return <div><h2 onClick={e => this.onExpandClick()}>{en.Title}</h2></div>
+			if (this.state.expanded) {
+				return <div>
+					<h2 onClick={e => this.onExpandClick(false)}>{en.Title}
+						<button onClick={e => this.onEditClick(true)}>Change</button>
+					</h2>
+					<div dangerouslySetInnerHTML={{__html: en.HTML}} />
+				</div>
+			} else {
+				return <div><h2 onClick={e => this.onExpandClick(true)}>{en.Title}</h2></div>
+			}
 		}
 	} // end of render function
 } // end of class
