c3e65e174b69077ad2828a09ba2d3783d827c8dc
Sergei G <sergeig.public@gmail.com>
2017-02-22 20:48:47 -0800

    added fetch as external

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   externals: {
     'react': 'React',
     'react-dom': 'ReactDOM',
-    'react-router': 'ReactRouter'
+    'react-router': 'ReactRouter',
+    'fetch': 'Fetch'
   }
 }
